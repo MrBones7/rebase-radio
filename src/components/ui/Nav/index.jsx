c454c173f0013f1ecf21e8f83a863d86692ae811
rbubs <rb758070@gmail.com>
2020-10-22 16:51:27 -0700

    Set nav root url

--- src/components/ui/Nav/index.jsx
+++ src/components/ui/Nav/index.jsx
@@ -3,13 +3,17 @@ import { nav, contactNav, homeNav } from './Nav.styles.less';
 
 const logoSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/favicon.png';
 
-const Nav = () => (
-  <div className={nav}>
-    <a href="#" className={homeNav}>
-      <img src={logoSrc} alt="Home" />
-    </a>
-    <a href="mailto:bones@rebase.radio" target="_blank" rel="noopener noreferrer" className={contactNav}>Contact</a>
-  </div>
-);
+const Nav = () => {
+  const rootUrl = window.location.href;
+
+  return (
+    <div className={nav}>
+      <a href={rootUrl} className={homeNav}>
+        <img src={logoSrc} alt="Home" />
+      </a>
+      <a href="mailto:bones@rebase.radio" target="_blank" rel="noopener noreferrer" className={contactNav}>Contact</a>
+    </div>
+  );
+};
 
 export default Nav;
