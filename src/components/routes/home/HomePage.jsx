f5c3b27b82a3803b148e0294aa0d06cc1015ec37
rbubs <rb758070@gmail.com>
2020-10-21 22:45:11 -0700

    Add play/pause controls

--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -1,45 +1,81 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from 'react';
-import { overlay, content, nav, player, contactNav, homeNav, fullwidthContainer, fullwidthVideo, wrapperFullwidthVideo } from './HomePage.styles.less';
+import React, { useState } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import cx from 'classnames';
+import { overlay, content, nav, playerControls, button, paused, contactNav, homeNav, fullwidthContainer, fullwidthVideo, wrapperFullwidthVideo } from './HomePage.styles.less';
 
-export default function HomePage() {
-  const faviconSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/favicon.png';
-  const backgroundVideoSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/background.mp4';
-  const posterSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/poster.jpg';
+const radioSrc = 'https://s4.radio.co/sb5955894a/listen';
+const faviconSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/favicon.png';
+const backgroundVideoSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/background.mp4';
+const posterSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/poster.jpg';
 
-  const Player = () => (
-    <div className={player}>
-      <p>Player controls</p>
-    </div>
-  );
+const PlayerControls = ({ onPlay, onPause }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  const Nav = () => (
-    <div className={nav}>
-      <a href="/" className={homeNav}>
-        <img src={faviconSrc} alt="Home" />
-      </a>
-      <a href="mailto:bones@rebase.radio" target="_blank" rel="noopener noreferrer" className={contactNav}>Contact</a>
-    </div>
-  );
-
-  const VideoBackground = (
-    <div className={fullwidthContainer}>
-      <div className={wrapperFullwidthVideo}>
-        <video loop muted playsInline autoPlay poster={posterSrc} className={fullwidthVideo}>
-          <source src={backgroundVideoSrc} type="video/mp4" />
-          <img src={posterSrc} alt="Your browser does not support the video element" />
-        </video>
-      </div>
-    </div>
-  );
+  const togglePlay = () => {
+    if (isPlaying) {
+      onPause();
+      setIsPlaying(false);
+    } else {
+      onPlay();
+      setIsPlaying(true);
+    }
+  };
 
   return (
-    <div className={content}>
-      <div className={overlay}>
-        <Nav />
-        <Player />
-      </div>
-      {VideoBackground}
+    <div className={playerControls}>
+      <button onClick={togglePlay} className={isPlaying ? cx(button, paused) : cx(button)} />
     </div>
   );
+};
+
+const Nav = () => (
+  <div className={nav}>
+    <a href="/" className={homeNav}>
+      <img src={faviconSrc} alt="Home" />
+    </a>
+    <a href="mailto:bones@rebase.radio" target="_blank" rel="noopener noreferrer" className={contactNav}>Contact</a>
+  </div>
+);
+
+const VideoBackground = (
+  <div className={fullwidthContainer}>
+    <div className={wrapperFullwidthVideo}>
+      <video loop muted playsInline autoPlay poster={posterSrc} className={fullwidthVideo}>
+        <source src={backgroundVideoSrc} type="video/mp4" />
+        <img src={posterSrc} alt="Your browser does not support the video element" />
+      </video>
+    </div>
+  </div>
+);
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.player = null;
+  }
+
+  render() {
+    const playerEl = () => this.player && this.player.audioEl.current;
+    const onPlay = () => playerEl() && playerEl().play();
+    const onPause = () => playerEl() && playerEl().pause();
+
+    return (
+      <div className={content}>
+        <div className={overlay}>
+          <Nav />
+          <PlayerControls onPause={onPause} onPlay={onPlay} />
+          <ReactAudioPlayer
+            src={radioSrc}
+            controls
+            preload
+            ref={(element) => { this.player = element; }}
+          />
+        </div>
+        {VideoBackground}
+      </div>
+    );
+  }
 }
+
+export default HomePage;
