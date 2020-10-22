603b62a9fa25c5b03db1e2d1f20e27d317d8b948
rbubs <rb758070@gmail.com>
2020-10-21 19:27:24 -0700

    use static-assets folder

--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -1,18 +1,53 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
-import { fullscreenBg, fullscreenBgVideo } from './HomePage.styles.less';
+import { overlay, content, fullwidthContainer, fullwidthVideo, wrapperFullwidthVideo } from './HomePage.styles.less';
 
 import PosterImage from './poster.jpg';
 
 export default function HomePage() {
-  const backgroundVideoSrc = "https://github.com/rbubs/rebase-radio/raw/master/video-asset/background.mp4";
+  const faviconSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/favicon.png';
+  const backgroundVideoSrc = 'https://github.com/rbubs/rebase-radio/raw/master/static-assets/background.mp4';
+
+  const homeLinkStyle = {
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+  };
+
+  const contactStyle = {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+    color: 'white',
+    textDecoration: 'none',
+    fontSize: '2rem',
+    fontWeight: 'bold',
+  };
+
+  const Overlay = (
+    <div className={overlay}>
+      <a href="/" style={homeLinkStyle}>
+        <img src={faviconSrc} alt="Home" />
+      </a>
+      <a href="mailto:bones@rebase.radio" rel="noreferer" target="_blank" style={contactStyle}>Contact</a>
+    </div>
+  );
+
+  const VideoBackground = (
+    <div className={fullwidthContainer}>
+      <div className={wrapperFullwidthVideo}>
+        <video loop muted autoPlay poster={PosterImage} className={fullwidthVideo}>
+          <source src={backgroundVideoSrc} type="video/mp4" />
+          <img src={PosterImage} alt="Your browser does not support the video element" />
+        </video>
+      </div>
+    </div>
+  );
 
   return (
-    <div className={fullscreenBg}>
-      <video loop muted autoPlay poster={PosterImage} className={fullscreenBgVideo}>
-        <source src={backgroundVideoSrc} type="video/mp4" />
-        <img src={PosterImage} alt="Your browser does not support the video element" />
-      </video>
+    <div className={content}>
+      {Overlay}
+      {VideoBackground}
     </div>
   );
 }
