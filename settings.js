ddd4e2cd238aeeb113e936418b8815d18d1fa1e7
MrBones7 <73261221+MrBones7@users.noreply.github.com>
2020-10-23 00:28:48 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ module.exports = {
   // use cname option to add CNAME file to webpack build
   // CNAME file allows to use custom domain names with gh-pages, example:
   // cname: 'omatsuri.app'
-  cname: null,
+  cname: www.rebase.radio,
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: '/rebase-radio',
+  repoPath: null,
 
   // list of routes that should be prerendered
   // list should always contain '/' and '/404' for gh-pages to recognize app
